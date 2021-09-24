cd266832618ae0a8ea38119e7c859a3815aafb8c
Poyap <ryougiako@gmail.com>
2021-09-24 16:35:05 +0800

    添加柱状图渐变颜色

--- src/components/chart-2.tsx
+++ src/components/chart-2.tsx
@@ -11,31 +11,63 @@ export const Chart2 = () =>{
         myChart.setOption(createEchartsOptions({
             xAxis: {
                 type:'value',
-                boundaryGap: [0, 0.01]
+                boundaryGap: [0, 0.01],
+                splitLine:{show: false},
+                axisLabel:{show: false}
             },
             yAxis: {
+                axisTick:{show: false},
                 type:'category',
-                data:['示例', '示例', '示例', '示例', '示例', '示例', '示例']
+                data:['某某公安局', '某某公安局', '某某公安局', '某某公安局', '某某公安局', '某某公安局', '某某公安局', '某某公安局', '某某公安局'],
+                axisLabel:{
+                    formatter(val){
+                        return val.replace('公安局','\n公安局');
+                    }
+                }
             },
             series: [
                 {
                     name: '2020年',
                     type: 'bar',
-                    data: [3221, 1125, 3210, 3418, 1230, 1235, 4524, 2341, 1228]
+                    data: [3221, 1125, 3210, 3418, 1230, 1235, 4524, 2341, 1228],
+                    itemStyle:{
+                        normal:{
+                            color: new echarts.graphic.LinearGradient(0,0,1,0,[{
+                                offset:0,
+                                color:'#2034f9'
+                            },{
+                                offset:1,
+                                color:'#04a1ff'
+                            }])
+                        }
+                    }
                 },
                 {
                     name: '2021年',
                     type: 'bar',
-                    data: [1125, 3221, 3210, 1235, 3418, 2341, 1230, 1228, 4524]
+                    data: [1125, 3221, 3210, 1235, 3418, 2341, 1230, 1228, 4524],
+                    itemStyle:{
+                        normal:{
+                            color: new echarts.graphic.LinearGradient(0,0,1,0,[{
+                                offset:0,
+                                color:'#b92ae8'
+                            },{
+                                offset:1,
+                                color:'#6773e7'
+                            }])
+                        }
+                    }
                 },
             ]
         }));
     },[])
     return (
-        <div className="bordered 管辖统计">
+        <div className="bordered 破获排名">
             <h2>案件破获排名</h2>
-            <div ref={divRef} className="chart">
-
+            <div ref={divRef} className="chart"/>
+            <div className="legend">
+                <span className="first"/> 破案排名2021
+                <span className="second"/> 破案排名2020
             </div>
         </div>
     )
